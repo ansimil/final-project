c0d7a941fb404480dc4b51ce8aa85c4ec1cbdc8f
John Doe <AndrewSimonMiller@andrews-mbp.localdomain>
2022-10-09 12:18:50 +0200

    added stock check at checkout

--- src/components/CheckoutBtn.jsx
+++ src/components/CheckoutBtn.jsx
@@ -6,11 +6,40 @@ import {loadStripe}  from '@stripe/stripe-js'
 
 const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLIC}`)
 
-const CheckoutBtn = () => {
+const CheckoutBtn = ({ outOfStock }) => {
     
     const { cartDetails } = useShoppingCart()
 
     const handleCheckout = async () => {
+        if (outOfStock.length > 0) {
+          let str = ''
+          outOfStock.forEach((module, i) => {
+            str = str + ` ${i+1}. Module: ${module[0]}(${module[1]})`
+          })
+          toast.error(<span>{`The following item(s) don't have enough stock:`}<ul>
+            {outOfStock.map((module, i) => {
+             return ( <li>{`${module[0]}(${module[1]})`}</li> )
+            })}
+          </ul>Please adjust your cart accordingly</span>, {
+            duration: 7000,
+            style: {
+                    border: '2px solid black',
+                    backgroundColor: 'white',
+                    borderRadius: '0px',
+                    padding: '5px 10px',
+                    color: 'black',
+                    textAlign: 'center',
+                    lineHeight: '20px'
+           },
+           iconTheme: {
+                    primary: '#000',
+                    secondary: '#fff',
+          },
+          })
+        }
+
+        else if (outOfStock.length === 0) {
+
         const stripe = await stripePromise;
         const session = await axios.post(`${process.env.REACT_APP_API_URL}/checkout-session`, cartDetails)
         .then(res => res.data)
@@ -24,6 +53,7 @@ const CheckoutBtn = () => {
             console.log(session)
             stripe.redirectToCheckout({ sessionId: id });
         }
+      }
 
         return handleCheckout;
     }
