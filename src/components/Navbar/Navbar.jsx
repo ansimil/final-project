471ac54febea8f1744ae1c695764b9fa2bfa374c
John Doe <andrewsimonmiller@gmail.com>
2023-01-17 08:23:33 +1100

    adding beginnings of hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,16 +6,56 @@ import ProfileBtn from '../ProfileBtn'
 import DashboardBtn from '../DashboardBtn'
 import CartBtn from '../CartBtn'
 import { AuthContext } from '../../contexts/auth'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { useShoppingCart } from 'use-shopping-cart';
+import hamburgerIcon from '../../assets/hamburger-icon.png'
+import closeIcon from '../../assets/close-icon.png'
 import './Navbar.css'
 
 
+const NavMiddle = ({isLoggedIn, user, classname}) => {
+  let class1, class2
+  if (classname === 'noHamburger') {
+    class1 = 'navMiddle'
+    class2 = 'navMiddleLoggedOut'
+  }
+  else if (classname === 'hamburger'){
+    class1 = 'navMiddleHamburger'
+    class2 = 'navMiddleHamburgerLoggedOut'
+  }
+  return (
+    <div className={isLoggedIn ? class1 : class2}>
+      <div className="navBarMiddleInnerLink">
+        <Link className="navBtn modulesBtn" to="/modules">Modules</Link>
+      </div>
+      <div className="navBarMiddleInnerLink">
+      <Link className="navBtn modulesBtn" to="/contact">Contact</Link>
+      </div>
+      <div className="navBarMiddleInnerLink">
+      <Link className="navBtn modulesBtn" to="/about">About</Link>
+      </div>
+    {isLoggedIn && user.isAdmin && <DashboardBtn />}
+    </div>
+  )
+}
 
+
+const HamburgerMenu = ({ hamburgerOpen, setHamburgerOpen }) => {
+
+    return (
+      <div className='hamburger'>
+      <div>
+      {!hamburgerOpen && <img onClick={()=>{setHamburgerOpen(!hamburgerOpen)}} className='hamburgerIcon' src={hamburgerIcon} alt="hamburger" />}
+      {hamburgerOpen && <img onClick={()=>{setHamburgerOpen(!hamburgerOpen)}} className='closeIcon' src={closeIcon} alt="hamburger" />}
+      </div>
+      {hamburgerOpen && <NavMiddle classname={'hamburger'}/>}
+      </div>
+    )
+}
 
 
 const Navbar = () => {
- 
+  const [hamburgerOpen, setHamburgerOpen] = useState(false)
   const { cartCount } = useShoppingCart()
   const { isLoggedIn, user } = useContext(AuthContext)
 
@@ -23,25 +63,14 @@ const Navbar = () => {
     <div className="navbar">
     
         <HomeBtn/>
-
-        <div className={isLoggedIn ? 'navMiddle' : 'navMiddleLoggedOut'}>
-        <div className="navBarMiddleInnerLink">
-          <Link className="navBtn modulesBtn" to="/modules">Modules</Link>
-        </div>
-        <div className="navBarMiddleInnerLink">
-        <Link className="navBtn modulesBtn" to="/contact">Contact</Link>
-        </div>
-        <div className="navBarMiddleInnerLink">
-        <Link className="navBtn modulesBtn" to="/about">About</Link>
-        </div>
-        {isLoggedIn && user.isAdmin && <DashboardBtn />}
-        </div>
+        <NavMiddle isLoggedIn={isLoggedIn} user={user} classname={'noHamburger'}/>
+        <HamburgerMenu setHamburgerOpen={setHamburgerOpen} hamburgerOpen={hamburgerOpen}/>
 
         <div className='cartSigninLogout'>
-        {isLoggedIn && cartCount > 0 && <CartBtn/>}
-        {isLoggedIn && <ProfileBtn />}
-        {!isLoggedIn && <SignInBtn/>}
-        {isLoggedIn && <LogoutBtn />}
+          {isLoggedIn && cartCount > 0 && <CartBtn/>}
+          {isLoggedIn && <ProfileBtn />}
+          {!isLoggedIn && <SignInBtn/>}
+          {isLoggedIn && <LogoutBtn />}
         </div>
 
     </div>
