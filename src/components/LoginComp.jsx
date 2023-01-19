a9f7a10bf5f7125d6e08b07ee14b65dd4b0838c6
John Doe <andrewsimonmiller@gmail.com>
2023-01-20 08:29:57 +1100

    changed design of singup page to only show one switchable form. Added optimization for mobile

--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -1,67 +1,25 @@
 import React from 'react'
-import { useState, useContext, useEffect } from 'react'
+import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
 import { AuthContext } from '../contexts/auth'
 import { useShoppingCart } from 'use-shopping-cart'
+import { useForm } from "react-hook-form";
+
+import axios from 'axios'
+
 
 const LoginComp = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
     const { loadCart } = useShoppingCart()
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState(undefined);
-
-    const [emailCheck, setEmailCheck] = useState(true);
-    const [passwordCheck, setPasswordCheck] = useState(true);
     
     const navigate = useNavigate();
     const { storeToken, authenticateUser } = useContext(AuthContext);
-
-
-    useEffect(() => {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-      const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-
-      if (!emailRegex.test(email)){
-        setEmailCheck(false)    
-      }
-      
-      if (!passwordRegex.test(password)){
-          setPasswordCheck(false)    
-      }
-        
-      if (email.length === 0){
-          setEmailCheck(true)
-      }
-
-      if (password.length === 0){
-        setPasswordCheck(true)
-      }
-
-      if (emailRegex.test(email)) {
-        setEmailCheck(true)
-      }
-
-      else if (passwordRegex.test(password)) {
-        setPasswordCheck(true)
-      }
-
-    }, [email, password])
-   
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    };
-
-    const handlePassword = (e) => {
-        setPassword(e.target.value)   
-    };
-   
     
-    const handleLoginSubmit = (e) => {
+    const handleLoginSubmit = (data, e) => {
       e.preventDefault();
-      const requestBody = { email, password };
-   
-      axios.post(`${process.env.REACT_APP_API_URL}/login`, requestBody)
+      reset()
+      axios.post(`${process.env.REACT_APP_API_URL}/login`, data)
         .then(async (response) => {
           storeToken(response.data.authToken) 
           await authenticateUser()
@@ -79,25 +37,24 @@ const LoginComp = () => {
       <div className="LoginPage">
         <h2>Login</h2>
    
-        <form className="loginForm" onSubmit={handleLoginSubmit}>
+        <form className="loginForm" onSubmit={handleSubmit(handleLoginSubmit)}>
           <label>Email:</label>
           <input 
-            className={!emailCheck ? "signupFormRed" : "regular"} 
+            className={errors.email ? "signupFormRed" : "regular"} 
             type="email"
             name="email"
-            value={email}
-            onChange={handleEmail}
+            {...register("email", { required: true,  pattern: {value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/, message: "Please enter a valid email address"}})}
           />
-          
+          { errors.email && <p className="error-message">{errors.email.message}</p> }
    
           <label>Password:</label>
           <input
-            className={!passwordCheck ? "signupFormRed" : "regular"} 
+            className={errors.password ? "signupFormRed" : "regular"} 
             type="password"
             name="password"
-            value={password}
-            onChange={handlePassword}
+            {...register("password", { required: true, pattern: {value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/, message:  "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter."}})}
           />
+          { errors.password && <p className="error-message">{errors?.password?.message}</p> }
         
   
           <br/>
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 import SignupComp from '../../components/SignupComp'
 import LoginComp from '../../components/LoginComp'
 import './Signup.css'
 
 const Signup = () => {
+  const [signUp, setSignUp] = useState(true)
   return (
     <div className="signupContainer">
         <div className="signupInnerDiv">
-          <SignupComp/>
-          <LoginComp/>
+        <div className="signUpLoginBtns">
+          <button className={signUp && "selected"} onClick={()=>{setSignUp(true)}}>Sign up</button>
+          <button className={!signUp && "selected"} onClick={()=>{setSignUp(false)}}>Login</button>
+        </div>
+          {signUp && <SignupComp/>}
+          {!signUp && <LoginComp/>}
         </div>
     </div>
   )
